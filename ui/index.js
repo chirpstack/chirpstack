08ca7f39a8a88f21464f70df4f786229b0fc2152
Victor Mu <victor@helium.com>
2022-08-04 13:27:49 -0700

    Dockerize express server to serve up custom main bundle

---
+++ ui/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const fs = require('fs')
+
+const app = express()
+const port = 3000
+
+app.get('/js/*', (req, res) => {
+  fs.readdir(`${__dirname}/build/static/js`, (err, files) => {
+    if (err) {
+      console.log("Main js bundle not created, check yarn build in Dockerfile")
+    } else {
+      const fileName = files.find(file => file.match(/main\..+\.js/))
+      
+      res.sendFile(`${__dirname}/build/static/js/${fileName}`)
+    }
+  })
+  
+})
+
+app.listen(port, () => {
+  console.log(`Nova UI bundle serving on port ${port}`)
+})
