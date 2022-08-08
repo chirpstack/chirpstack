3ee7f6f3b01afcf07c9119114350f88b8adc4131
Alejandra Buznego <abuznego23@gmail.com>
2022-08-08 14:41:54 -0500

    pass request to CLS for inviting user

--- ui/src/views/tenants/CreateTenantUser.tsx
+++ ui/src/views/tenants/CreateTenantUser.tsx
@@ -1,27 +1,79 @@
 import React, { Component } from "react";
+import { notification } from "antd";
 import { Link, RouteComponentProps } from "react-router-dom";
 
 import { Space, Breadcrumb, Card, PageHeader } from "antd";
 
-import { Tenant, TenantUser, AddTenantUserRequest } from "@chirpstack/chirpstack-api-grpc-web/api/tenant_pb";
+import { Tenant, TenantUser } from "@chirpstack/chirpstack-api-grpc-web/api/tenant_pb";
+import { User } from "@chirpstack/chirpstack-api-grpc-web/api/user_pb";
 
 import TenantUserForm from "./TenantUserForm";
-import TenantStore from "../../stores/TenantStore";
+
+// stores
+import SessionStore from "../../stores/SessionStore";
 
 interface IProps extends RouteComponentProps {
   tenant: Tenant;
 }
 
-class CreateTenantUser extends Component<IProps> {
-  onFinish = (obj: TenantUser) => {
-    obj.setTenantId(this.props.tenant.getId());
+interface IState {
+  user?: User;
+}
 
-    let req = new AddTenantUserRequest();
-    req.setTenantUser(obj);
+class CreateTenantUser extends Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
 
-    TenantStore.addUser(req, () => {
-      this.props.history.push(`/tenants/${this.props.tenant.getId()}/users`);
+    this.state = {
+      user: undefined,
+    };
+  }
+
+  componentDidMount() {
+    SessionStore.on("change", () => {
+      this.setState({
+        user: SessionStore.getUser(),
+      });
     });
+
+    this.setState({
+      user: SessionStore.getUser(),
+    });
+  }
+
+  onFinish = async (obj: TenantUser) => {
+    // Highjack the request to login server
+    if (this.state.user) {
+      const res = await fetch("http://localhost:4000/api/invite_user", {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify({
+          email: obj.getEmail(),
+          tenant_id: this.props.tenant.getId(),
+          tenant_name: this.props.tenant.getName(),
+          admin: obj.getIsAdmin(),
+          inviter_id: this.state.user.getId(),
+        }),
+      });
+
+      if (res.status === 201) {
+        notification.success({
+          message: "User invited",
+          duration: 3,
+        });
+
+        this.props.history.push(`/tenants/${this.props.tenant.getId()}/users`);
+      } else {
+        // TODO figure out why error msg doesn't come in
+        notification.error({
+          message: "Unable to invite user, please try again",
+          duration: 3,
+        });
+      }
+    }
   };
 
   render() {
