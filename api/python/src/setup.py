4b41893f4289a18fecd574d95d0bfbe6e383f0d2
Orne Brocaar <info@brocaar.com>
2023-02-28 12:07:16 +0000

    Bump version to 4.3.0-test.2

--- api/python/src/setup.py
+++ api/python/src/setup.py
@@ -18,7 +18,7 @@ CLASSIFIERS = [
 
 setup(
     name='chirpstack-api',
-    version = "4.3.0-test.1",
+    version = "4.3.0-test.2",
     url='https://github.com/brocaar/chirpstack-api',
     author='Orne Brocaar',
     author_email='info@brocaar.com',
