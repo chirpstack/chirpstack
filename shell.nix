c815c6be88a38843355b4db1678cf8e095dc770c
Orne Brocaar <info@brocaar.com>
2024-03-13 09:43:37 +0000

    Update Rust + dev env.

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-23.05.tar.gz") {} }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-23.11.tar.gz") {} }:
 
 pkgs.mkShell {
   nativeBuildInputs = [
