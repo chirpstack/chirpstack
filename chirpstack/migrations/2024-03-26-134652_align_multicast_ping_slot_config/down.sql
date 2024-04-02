161cd9008c2345f1466f3f41430c9f81aa147563
Orne Brocaar <info@brocaar.com>
2024-04-02 09:25:58 +0100

    Fix multicast class-b ping-slot migration in case value is 0.

--- chirpstack/migrations/2024-03-26-134652_align_multicast_ping_slot_config/down.sql
+++ chirpstack/migrations/2024-03-26-134652_align_multicast_ping_slot_config/down.sql
@@ -1,7 +1,7 @@
 alter table multicast_group
   alter column class_b_ping_slot_nb_k type integer;
 
-update multicast_group set class_b_ping_slot_nb_k = pow(2, class_b_ping_slot_nb_k) * 32;
+update multicast_group set class_b_ping_slot_nb_k = coalesce(pow(2, nullif(class_b_ping_slot_nb_k, 0)) * 32, 0);
 
 alter table multicast_group
   rename column class_b_ping_slot_nb_k to class_b_ping_slot_period;
--- chirpstack/migrations/2024-03-26-134652_align_multicast_ping_slot_config/up.sql
+++ chirpstack/migrations/2024-03-26-134652_align_multicast_ping_slot_config/up.sql
@@ -1,7 +1,7 @@
 alter table multicast_group
   rename column class_b_ping_slot_period to class_b_ping_slot_nb_k;
 
-update multicast_group set class_b_ping_slot_nb_k = log(2, class_b_ping_slot_nb_k / 32);
+update multicast_group set class_b_ping_slot_nb_k = coalesce(log(2, nullif(class_b_ping_slot_nb_k, 0) / 32), 0);
 
 alter table multicast_group
   alter column class_b_ping_slot_nb_k type smallint;
