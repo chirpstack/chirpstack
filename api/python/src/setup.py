c7f1367bf85648098ff1d6258e58738ceaa1d5ae
Orne Brocaar <info@brocaar.com>
2024-05-16 11:52:35 +0100

    api: Remove frame_log from __init__.py (Python).
    Closes #422.

--- api/python/src/setup.py
+++ api/python/src/setup.py
@@ -18,7 +18,7 @@ CLASSIFIERS = [
 
 setup(
     name='chirpstack-api',
-    version = "4.8.1",
+    version = "4.8.1.post1",
     url='https://github.com/brocaar/chirpstack-api',
     author='Orne Brocaar',
     author_email='info@brocaar.com',
