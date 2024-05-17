2a109a9ea1f413600b6c1532b6ee05bffea1f17b
Mohamed Belaouad <mohamed.belaouad@wattsense.com>
2024-05-17 17:48:17 +0200

    storage: Introduce DB agnostic sql_types in fields

--- chirpstack/src/storage/fields/mod.rs
+++ chirpstack/src/storage/fields/mod.rs
@@ -5,3 +5,27 @@ mod multicast_group_scheduling_type;
 pub use key_value::KeyValue;
 pub use measurements::*;
 pub use multicast_group_scheduling_type::MulticastGroupSchedulingType;
+
+pub mod sql_types {
+    // TimestampTz is represented differently in Diesel
+    // but it can essentially convert from/to chrono::*DateTime*
+    #[cfg(feature = "postgres")]
+    pub type Timestamptz = diesel::sql_types::Timestamptz;
+    #[cfg(feature = "sqlite")]
+    pub type Timestamptz = diesel::sql_types::TimestamptzSqlite;
+
+    // Sqlite has no native json type so use text
+    #[cfg(feature = "postgres")]
+    pub type JsonT = diesel::sql_types::Jsonb;
+    // TODO: sqlite is adding "jsonb" support, different from postgres
+    // So we may switch the column to blob?
+    // see https://sqlite.org/draft/jsonb.html
+    #[cfg(feature = "sqlite")]
+    pub type JsonT = diesel::sql_types::Text;
+
+    // Sqlite has no native json type so use text
+    #[cfg(feature = "postgres")]
+    pub type Uuid = diesel::sql_types::Uuid;
+    #[cfg(feature = "sqlite")]
+    pub type Uuid = diesel::sql_types::Text;
+}
