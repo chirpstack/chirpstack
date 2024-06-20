c7efc94172d5a4630f7f1f7374c60d46112d4392
Orne Brocaar <info@brocaar.com>
2024-06-20 15:07:21 +0100

    Define one sql_types module per feature.
    This removes some #[cfg(feature ...)] duplication, and makes the code a
    bit easier to read (for me at least).

--- chirpstack/src/storage/fields/mod.rs
+++ chirpstack/src/storage/fields/mod.rs
@@ -14,26 +14,24 @@ pub use measurements::*;
 pub use multicast_group_scheduling_type::MulticastGroupSchedulingType;
 pub use uuid::Uuid;
 
+#[cfg(feature = "postgres")]
 pub mod sql_types {
-    // TimestampTz is represented differently in Diesel
-    // but it can essentially convert from/to chrono::*DateTime*
-    #[cfg(feature = "postgres")]
     pub type Timestamptz = diesel::sql_types::Timestamptz;
-    #[cfg(feature = "sqlite")]
+
+    pub type JsonT = diesel::sql_types::Jsonb;
+
+    pub type Uuid = diesel::sql_types::Uuid;
+}
+
+#[cfg(feature = "sqlite")]
+pub mod sql_types {
     pub type Timestamptz = diesel::sql_types::TimestamptzSqlite;
 
-    // Sqlite has no native json type so use text
-    #[cfg(feature = "postgres")]
-    pub type JsonT = diesel::sql_types::Jsonb;
     // TODO: sqlite is adding "jsonb" support, different from postgres
     // So we may switch the column to blob?
     // see https://sqlite.org/draft/jsonb.html
-    #[cfg(feature = "sqlite")]
     pub type JsonT = diesel::sql_types::Text;
 
     // Sqlite has no native json type so use text
-    #[cfg(feature = "postgres")]
-    pub type Uuid = diesel::sql_types::Uuid;
-    #[cfg(feature = "sqlite")]
     pub type Uuid = diesel::sql_types::Text;
 }
