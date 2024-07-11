3e5239ba2da793278a317469c3167d9c764ad474
Bernd Storath <bernd.storath@offizium.de>
2024-07-11 09:24:57 +0200

    make it more like old version

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -18,10 +18,14 @@ import "./index.css";
 
 Chart.register(MatrixController, MatrixElement, ...registerables);
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
