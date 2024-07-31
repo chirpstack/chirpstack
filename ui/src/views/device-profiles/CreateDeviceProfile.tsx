36f5375ef331e1e2da1f9d40205a4a7afa162874
Bernd Storath <bernd.storath@offizium.de>
2024-07-31 13:05:05 +0200

    fix indent, fix wrong types & text

--- ui/src/views/device-profiles/CreateDeviceProfile.tsx
+++ ui/src/views/device-profiles/CreateDeviceProfile.tsx
@@ -31,38 +31,38 @@ function CreateDeviceProfile(props: IProps) {
   };
 
   const codecScript = `/**
-   * Decode uplink function
-   * 
-   * @param {object} input
-   * @param {number[]} input.bytes Byte array containing the uplink payload, e.g. [255, 230, 255, 0]
-   * @param {number} input.fPort Uplink fPort.
-   * @param {Record<string, string>} input.variables Object containing the configured device variables.
-   * 
-   * @returns {{data: Record<string, any>}} Object representing the decoded payload.
-   */
-  function decodeUplink(input) {
-    return {
-      data: {
-        // temp: 22.5
-      }
-    };
-  }
-  
-  /**
-   * Encode downlink function.
-   * 
-   * @param {object} input
-   * @param {Record<string, any>} input.data Object representing the payload that must be encoded.
-   * @param {number} input.variables Object containing the configured device variables.
-   * 
-   * @returns {{bytes?: number[]}} Object representing the decoded payload.
-   */
-  function encodeDownlink(input) {
-    return {
-      // bytes: [225, 230, 255, 0]
-    };
-  }
-  `;
+  * Decode uplink function
+  * 
+  * @param {object} input
+  * @param {number[]} input.bytes Byte array containing the uplink payload, e.g. [255, 230, 255, 0]
+  * @param {number} input.fPort Uplink fPort.
+  * @param {Record<string, string>} input.variables Object containing the configured device variables.
+  * 
+  * @returns {{data: Record<string, any>}} Object representing the decoded payload.
+  */
+function decodeUplink(input) {
+  return {
+    data: {
+      // temp: 22.5
+    }
+  };
+}
+
+/**
+ * Encode downlink function.
+ * 
+ * @param {object} input
+ * @param {Record<string, any>} input.data Object representing the payload that must be encoded.
+ * @param {Record<string, string>} input.variables Object containing the configured device variables.
+ * 
+ * @returns {{bytes: number[]}} Byte array containing the downlink payload.
+ */
+function encodeDownlink(input) {
+  return {
+    // bytes: [225, 230, 255, 0]
+  };
+}
+`;
 
   const deviceProfile = new DeviceProfile();
   deviceProfile.setPayloadCodecScript(codecScript);
