20a52251ad119aa55d55546e88b93763a6e9cf0e
Orne Brocaar <info@brocaar.com>
2025-01-20 10:36:44 +0000

    api: Extend 'limit' field documentation.

--- api/go/api/device.pb.go
+++ api/go/api/device.pb.go
@@ -745,6 +745,7 @@ type ListDevicesRequest struct {
 	unknownFields protoimpl.UnknownFields
 
 	// Max number of devices to return in the result-set.
+	// If not set, it will be treated as 0, and the response will only return the total_count.
 	Limit uint32 `protobuf:"varint,1,opt,name=limit,proto3" json:"limit,omitempty"`
 	// Offset in the result-set (for pagination).
 	Offset uint32 `protobuf:"varint,2,opt,name=offset,proto3" json:"offset,omitempty"`
--- api/go/api/device_profile_template.pb.go
+++ api/go/api/device_profile_template.pb.go
@@ -735,6 +735,7 @@ type ListDeviceProfileTemplatesRequest struct {
 	unknownFields protoimpl.UnknownFields
 
 	// Max number of device-profile templates to return in the result-set.
+	// If not set, it will be treated as 0, and the response will only return the total_count.
 	Limit uint32 `protobuf:"varint,1,opt,name=limit,proto3" json:"limit,omitempty"`
 	// Offset in the result-set (for pagination).
 	Offset uint32 `protobuf:"varint,2,opt,name=offset,proto3" json:"offset,omitempty"`
