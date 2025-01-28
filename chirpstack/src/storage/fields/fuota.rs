c04d86786467c7b5d47a38a36f62968d8d847742
Orne Brocaar <info@brocaar.com>
2025-01-28 14:09:12 +0000

    Add tests + add fuota jobs functions.

--- chirpstack/src/storage/fields/fuota.rs
+++ chirpstack/src/storage/fields/fuota.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 use anyhow::Error;
 use diesel::backend::Backend;
 use diesel::sql_types::Text;
@@ -72,6 +74,85 @@ where
 
 #[cfg(feature = "sqlite")]
 impl serialize::ToSql<Text, Sqlite> for RequestFragmentationSessionStatus {
+    fn to_sql(&self, out: &mut serialize::Output<'_, '_, Sqlite>) -> serialize::Result {
+        out.set_value(String::from(self));
+        Ok(serialize::IsNull::No)
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize, AsExpression, FromSqlRow)]
+#[diesel(sql_type = Text)]
+pub enum FuotaJob {
+    McGroupSetup,
+    McSession,
+    FragSessionSetup,
+    Enqueue,
+    FragStatus,
+}
+
+impl fmt::Display for FuotaJob {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", String::from(self))
+    }
+}
+
+impl From<&FuotaJob> for String {
+    fn from(value: &FuotaJob) -> Self {
+        match value {
+            FuotaJob::McGroupSetup => "MC_GROUP_SETUP",
+            FuotaJob::McSession => "MC_SESSION",
+            FuotaJob::FragSessionSetup => "FRAG_SESSION_SETUP",
+            FuotaJob::Enqueue => "ENQUEUE",
+            FuotaJob::FragStatus => "FRAG_STATUS",
+        }
+        .to_string()
+    }
+}
+
+impl TryFrom<&str> for FuotaJob {
+    type Error = Error;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        Ok(match value {
+            "MC_GROUP_SETUP" => Self::McGroupSetup,
+            "MC_SESSION" => Self::McSession,
+            "FRAG_SESSION_SETUP" => Self::FragSessionSetup,
+            "ENQUEUE" => Self::Enqueue,
+            "FRAG_STATUS" => Self::FragStatus,
+            _ => return Err(anyhow!("Invalid FuotaJob value: {}", value)),
+        })
+    }
+}
+
+impl<DB> deserialize::FromSql<Text, DB> for FuotaJob
+where
+    DB: Backend,
+    *const str: deserialize::FromSql<Text, DB>,
+{
+    fn from_sql(value: <DB as Backend>::RawValue<'_>) -> deserialize::Result<Self> {
+        let string = <*const str>::from_sql(value)?;
+        Ok(Self::try_from(unsafe { &*string })?)
+    }
+}
+
+#[cfg(feature = "postgres")]
+impl serialize::ToSql<Text, diesel::pg::Pg> for FuotaJob
+where
+    str: serialize::ToSql<Text, diesel::pg::Pg>,
+{
+    fn to_sql<'b>(
+        &'b self,
+        out: &mut serialize::Output<'b, '_, diesel::pg::Pg>,
+    ) -> serialize::Result {
+        <str as serialize::ToSql<Text, diesel::pg::Pg>>::to_sql(
+            &String::from(self),
+            &mut out.reborrow(),
+        )
+    }
+}
+
+#[cfg(feature = "sqlite")]
+impl serialize::ToSql<Text, Sqlite> for FuotaJob {
     fn to_sql(&self, out: &mut serialize::Output<'_, '_, Sqlite>) -> serialize::Result {
         out.set_value(String::from(self));
         Ok(serialize::IsNull::No)
