9923e3181be1253f79aa98786be985242c98f0c2
Lukas Kirner <l.kirner@rational-online.com>
2025-04-29 15:30:18 +0200

    Also record downlink mac_commands on data downlink

--- chirpstack/src/downlink/data.rs
+++ chirpstack/src/downlink/data.rs
@@ -11,7 +11,7 @@ use tracing::{debug, span, trace, warn, Instrument, Level};
 use crate::api::backend::get_async_receiver;
 use crate::api::helpers::{FromProto, ToProto};
 use crate::backend::roaming;
-use crate::downlink::{classb, error::Error, helpers, tx_ack};
+use crate::downlink::{classb, error::Error, helpers, record_downlink_mac_commands, tx_ack};
 use crate::gpstime::{ToDateTime, ToGpsTime};
 use crate::storage;
 use crate::storage::{
@@ -1038,6 +1038,8 @@ impl Data {
             .await
             .context("Send downlink frame")?;
 
+        record_downlink_mac_commands(self.mac_commands.clone());
+        
         Ok(())
     }
 
