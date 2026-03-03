51c2a6af2fa8f380c030549d8276ae5d54e69fd0
Orne Brocaar <info@brocaar.com>
2026-03-03 10:42:55 +0000

    ui: Move navigate into useEffect.

--- ui/src/views/tenants/TenantRedirect.tsx
+++ ui/src/views/tenants/TenantRedirect.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useNavigate } from "react-router";
 
 import type { ListTenantsResponse } from "@chirpstack/chirpstack-api-grpc-web/api/tenant_pb";
@@ -9,20 +10,22 @@ import SessionStore from "../../stores/SessionStore";
 function TenantRedirect() {
   const navigate = useNavigate();
 
-  const tenantId = SessionStore.getTenantId();
+  useEffect(() => {
+    const tenantId = SessionStore.getTenantId();
 
-  if (tenantId !== "") {
-    navigate(`/tenants/${tenantId}`);
-  } else {
-    const req = new ListTenantsRequest();
-    req.setLimit(1);
+    if (tenantId !== "") {
+      navigate(`/tenants/${tenantId}`);
+    } else {
+      const req = new ListTenantsRequest();
+      req.setLimit(1);
 
-    TenantStore.list(req, (resp: ListTenantsResponse) => {
-      if (resp.getResultList().length !== 0) {
-        navigate(`/tenants/${resp.getResultList()[0].getId()}`);
-      }
-    });
-  }
+      TenantStore.list(req, (resp: ListTenantsResponse) => {
+        if (resp.getResultList().length !== 0) {
+          navigate(`/tenants/${resp.getResultList()[0].getId()}`);
+        }
+      });
+    }
+  });
 
   return null;
 }
